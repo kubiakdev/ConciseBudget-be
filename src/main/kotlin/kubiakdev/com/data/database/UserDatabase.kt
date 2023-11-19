4e44501ea49bf19289434fe808b2452f814899be
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-19 15:28:37 +0100

    Add the rest of methods for managing the UserDatabase.kt

--- src/main/kotlin/kubiakdev/com/data/database/UserDatabase.kt
+++ src/main/kotlin/kubiakdev/com/data/database/UserDatabase.kt
@@ -3,15 +3,21 @@ package kubiakdev.com.data.database
 import kubiakdev.com.data.database
 import kubiakdev.com.data.model.user.User
 import org.bson.types.ObjectId
-import org.litote.kmongo.Id
 import org.litote.kmongo.eq
 import org.litote.kmongo.id.toId
 
 private val collection = database.getCollection<User>("user")
 
-suspend fun getById(id: String): User? {
-    val bsonId: Id<String> = ObjectId(id).toId()
-    return collection.findOne(User::id eq bsonId)
-}
+suspend fun getById(id: String): User? = collection.findOne(User::id eq ObjectId(id).toId())
 
 suspend fun getByEmail(email: String): User? = collection.findOne(User::email eq email)
+
+suspend fun getByAuthUid(authUid: String): User? = collection.findOne(User::authUid eq authUid)
+
+suspend fun addUser(user: User) {
+    collection.insertOne(user)
+}
+
+suspend fun removeById(id: String) {
+    collection.deleteOne(User::id eq ObjectId(id).toId())
+}
