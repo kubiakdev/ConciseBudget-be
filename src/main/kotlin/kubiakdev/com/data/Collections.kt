414699cd5842f752e760d3cd5cef6b4b447a1586
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-19 12:51:19 +0100

    Add mongoConnection to client creation

--- src/main/kotlin/kubiakdev/com/data/Collections.kt
+++ src/main/kotlin/kubiakdev/com/data/Collections.kt
@@ -3,10 +3,11 @@ package kubiakdev.com.data
 import kubiakdev.com.data.model.friend.Friends
 import kubiakdev.com.data.model.transaction.Transaction
 import kubiakdev.com.data.model.user.User
+import kubiakdev.com.mongoConnection
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
-private val client = KMongo.createClient().coroutine
+private val client = KMongo.createClient(mongoConnection).coroutine
 private val database = client.getDatabase("test")
 
 val usersCollection = database.getCollection<User>()
