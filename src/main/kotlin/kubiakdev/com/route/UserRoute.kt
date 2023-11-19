3b8246fa14609e2c7f96822dd24335d4e5310dc6
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-19 13:01:33 +0100

    Change geting the user

--- src/main/kotlin/kubiakdev/com/route/UserRoute.kt
+++ src/main/kotlin/kubiakdev/com/route/UserRoute.kt
@@ -1,14 +1,25 @@
 package kubiakdev.com.route
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kubiakdev.com.data.database.getUserById
 
 fun Route.userRoutes() {
     route("/user/{id}") {
         get {
-            val id = call.parameters["id"]
-            call.respondText("test")
+            val id = call.parameters["id"]!!
+            val user = getUserById(id)
+            user?.let {
+                call.respond(
+                    HttpStatusCode.OK,
+                    "found",
+                )
+            } ?: call.respond(
+                HttpStatusCode.NotFound,
+                "not found",
+            )
         }
     }
 }
