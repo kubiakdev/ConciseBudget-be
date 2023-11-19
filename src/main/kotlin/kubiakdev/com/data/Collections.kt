ff8e2285d7976a7b98fbe69c576e1e16e0deb1e2
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-19 12:10:32 +0100

    Add Collections.kt

---
+++ src/main/kotlin/kubiakdev/com/data/Collections.kt
@@ -0,0 +1,14 @@
+package kubiakdev.com.data
+
+import kubiakdev.com.data.model.friend.Friends
+import kubiakdev.com.data.model.transaction.Transaction
+import kubiakdev.com.data.model.user.User
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
+
+private val client = KMongo.createClient().coroutine
+private val database = client.getDatabase("test")
+
+val usersCollection = database.getCollection<User>()
+val transactionsCollection = database.getCollection<Transaction>()
+val friendsCollection = database.getCollection<Friends>()
