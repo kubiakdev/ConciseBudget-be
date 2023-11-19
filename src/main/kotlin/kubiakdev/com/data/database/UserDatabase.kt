416b645d1f4f2c9de19be3f97313bcb366d4a9d4
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-19 14:00:28 +0100

    Add route for getting user by email

--- src/main/kotlin/kubiakdev/com/data/database/UserDatabase.kt
+++ src/main/kotlin/kubiakdev/com/data/database/UserDatabase.kt
@@ -2,7 +2,8 @@ package kubiakdev.com.data.database
 
 import kubiakdev.com.data.database
 import kubiakdev.com.data.model.user.User
+import org.litote.kmongo.eq
 
-private val collection = database.getCollection<User>()
+private val collection = database.getCollection<User>("user")
 
-suspend fun getUserById(id: String): User? = collection.findOneById(id)
+suspend fun getByEmail(email: String): User? = collection.findOne(User::email eq email)
--- src/main/kotlin/kubiakdev/com/route/UserRoute.kt
+++ src/main/kotlin/kubiakdev/com/route/UserRoute.kt
@@ -4,13 +4,13 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kubiakdev.com.data.database.getUserById
+import kubiakdev.com.data.database.getByEmail
 
 fun Route.userRoutes() {
-    route("/user/{id}") {
+    route("/user/{email}") {
         get {
-            val id = call.parameters["id"]!!
-            val user = getUserById(id)
+            val email = call.parameters["email"]!!
+            val user = getByEmail(email)
             user?.let {
                 call.respond(
                     HttpStatusCode.OK,
