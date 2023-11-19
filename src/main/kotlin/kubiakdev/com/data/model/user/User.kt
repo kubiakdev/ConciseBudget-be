c187837aecfc7b4d43d758915df7e402aba9d8f7
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-19 14:44:11 +0100

    Getting the user by id

--- src/main/kotlin/kubiakdev/com/data/model/user/User.kt
+++ src/main/kotlin/kubiakdev/com/data/model/user/User.kt
@@ -1,9 +1,10 @@
 package kubiakdev.com.data.model.user
 
 import org.bson.codecs.pojo.annotations.BsonId
+import org.litote.kmongo.Id
 
 data class User(
-    @BsonId val id: String,
+    @BsonId val id: Id<String>,
     val authUid: String,
     val email: String,
     val publicKey: String,
--- src/main/kotlin/kubiakdev/com/route/UserRoute.kt
+++ src/main/kotlin/kubiakdev/com/route/UserRoute.kt
@@ -5,8 +5,21 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kubiakdev.com.data.database.getByEmail
+import kubiakdev.com.data.database.getById
 
 fun Route.userRoutes() {
+    route("/user/{id}") {
+        get {
+            val id = call.parameters["id"]!!
+            val user = getById(id)
+            if (user != null) {
+                call.respond(HttpStatusCode.OK, user)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+    }
+
     route("/user/{email}") {
         get {
             val email = call.parameters["email"]!!
