1221b9ae6239b8fd186c86d0e3f82f139c0c02aa
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-20 08:49:55 +0100

    Add transaction routes to routing

--- src/main/kotlin/kubiakdev/com/plugin/Routing.kt
+++ src/main/kotlin/kubiakdev/com/plugin/Routing.kt
@@ -3,11 +3,13 @@ package kubiakdev.com.plugin
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import kubiakdev.com.route.friendsRoutes
+import kubiakdev.com.route.transactionRoutes
 import kubiakdev.com.route.userRoutes
 
 fun Application.configureRouting() {
     routing {
         userRoutes()
         friendsRoutes()
+        transactionRoutes()
     }
 }
