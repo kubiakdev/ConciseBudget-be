d361e71f7c0dc8d307f5e3dd9f2782914cf4c7cf
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-21 17:56:20 +0100

    Update dependencies by adding okhttp client

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.9.20"
     id("io.ktor.plugin") version "2.3.5"
+    kotlin("plugin.serialization") version "1.9.20"
 }
 
 group = "kubiakdev.com"
@@ -31,14 +32,16 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml:2.3.5")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Authentication
     implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("com.google.firebase:firebase-admin:9.2.0")
+
+    // OkHttp
+    implementation("io.ktor:ktor-client-okhttp:1.6.6")
 
     // Mongo
     implementation("org.litote.kmongo:kmongo:4.11.0")
     implementation("org.litote.kmongo:kmongo-coroutine:4.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-
-    // Firebase Authentication
-    implementation("com.google.firebase:firebase-admin:9.2.0")
-
 }
