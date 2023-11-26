368ca5513e125189a122f727804c8e0a8237cf78
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-26 11:57:56 +0100

    Add UserMapper.kt

---
+++ src/main/kotlin/kubiakdev/com/util/mapper/UserMapper.kt
@@ -0,0 +1,19 @@
+package kubiakdev.com.util.mapper
+
+import kubiakdev.com.data.database.model.user.UserEntity
+import kubiakdev.com.route.model.user.UserRouteModel
+import org.bson.types.ObjectId
+
+fun UserEntity.toRouteModel() = UserRouteModel(
+    id = id?.toString(),
+    authUid = authUid,
+    email = email,
+    publicKey = publicKey
+)
+
+fun UserRouteModel.toEntityModel() = UserEntity(
+    id = id?.let { ObjectId(it) },
+    authUid = authUid,
+    email = email,
+    publicKey = publicKey
+)
