2976d9906c17757d60b4e2c0b1dc554aecd82b47
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-11-27 20:24:34 +0100

    Update MongoClient.kt

--- src/main/kotlin/kubiakdev/com/data/database/MongoClient.kt
+++ src/main/kotlin/kubiakdev/com/data/database/MongoClient.kt
@@ -1,6 +1,7 @@
 package kubiakdev.com.data.database
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
+import mongoConnection
 
 private val client = MongoClient.create(mongoConnection)
 val database = client.getDatabase("test")
