e6e7890422da848b0a47e7934655ca96083a3050
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-12-10 12:31:19 +0100

    Add mappers for FriendsMapper.kt

--- src/main/kotlin/kubiakdev/com/util/mapper/FriendsMapper.kt
+++ src/main/kotlin/kubiakdev/com/util/mapper/FriendsMapper.kt
@@ -2,10 +2,13 @@ package kubiakdev.com.util.mapper
 
 import kubiakdev.com.data.database.model.friend.FriendEntity
 import kubiakdev.com.data.database.model.friend.FriendsEntity
+import kubiakdev.com.domain.model.friend.Friend
+import kubiakdev.com.domain.model.friend.Friends
 import kubiakdev.com.route.friends.v1.model.FriendRouteModel
 import kubiakdev.com.route.friends.v1.model.FriendsRouteModel
 import org.bson.types.ObjectId
 
+// todo to remove mappers from entities to routes
 fun FriendEntity.toRouteModel() = FriendRouteModel(userId = userId, username = username)
 
 fun FriendsEntity.toRouteModel() = FriendsRouteModel(
@@ -23,4 +26,20 @@ fun FriendsRouteModel.toEntityModel() = FriendsEntity(
 fun FriendRouteModel.toEntityModel() = FriendEntity(
     userId = userId,
     username = username,
+)
+
+fun FriendEntity.toDomainModel() = Friend(userId = userId, username = username)
+
+fun FriendsEntity.toDomainModel() = Friends(
+    id = id!!.toString(),
+    ownerId = ownerId,
+    friends = friends.map { it.toDomainModel() }
+)
+
+fun Friend.toRouteModel() = FriendRouteModel(userId = userId, username = username)
+
+fun Friends.toRouteModel() = FriendsRouteModel(
+    id = id,
+    ownerId = ownerId,
+    friends = friends.map { it.toRouteModel() }
 )
