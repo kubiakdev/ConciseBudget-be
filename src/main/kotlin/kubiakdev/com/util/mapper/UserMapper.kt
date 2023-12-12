0d43e0d02fb63190b938a5377eabfbb03ed02b7b
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-12-12 18:51:31 +0100

    Improve mappers for users

--- src/main/kotlin/kubiakdev/com/util/mapper/UserMapper.kt
+++ src/main/kotlin/kubiakdev/com/util/mapper/UserMapper.kt
@@ -1,18 +1,33 @@
 package kubiakdev.com.util.mapper
 
 import kubiakdev.com.data.database.model.user.UserEntity
+import kubiakdev.com.domain.model.user.User
 import kubiakdev.com.route.user.v1.model.UserRouteModel
 import org.bson.types.ObjectId
 
-fun UserEntity.toRouteModel() = UserRouteModel(
+fun UserEntity.toDomainModel() = User(
     id = id?.toString(),
     authUid = authUid,
     email = email,
     publicKey = publicKey
 )
 
-fun UserRouteModel.toEntityModel() = UserEntity(
+fun User.toEntityModel() = UserEntity(
     id = id?.let { ObjectId(it) },
+    authUid = authUid,
+    email = email,
+    publicKey = publicKey
+)
+
+fun User.toRouteModel() = UserRouteModel(
+    id = id?.toString(),
+    authUid = authUid,
+    email = email,
+    publicKey = publicKey
+)
+
+fun UserRouteModel.toDomainModel() = User(
+    id = id,
     authUid = authUid,
     email = email,
     publicKey = publicKey
