444ba0428aeb651c4c63551a920cc74f924aa5d1
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2023-12-21 15:45:23 +0100

    Remove redundant challenge

--- src/main/kotlin/kubiakdev/com/app/authentication/firebase/util/FirebaseAuthenticationProvider.kt
+++ src/main/kotlin/kubiakdev/com/app/authentication/firebase/util/FirebaseAuthenticationProvider.kt
@@ -1,12 +1,10 @@
 package kubiakdev.com.app.authentication.firebase.util
 
 import com.google.firebase.auth.FirebaseToken
-import io.ktor.http.auth.*
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
-import kubiakdev.com.app.authentication.firebase.util.AuthenticationConst.AUTH_SCHEME
-import kubiakdev.com.app.authentication.firebase.util.AuthenticationConst.FIREBASE_AUTH_CONFIGURATION_NAME
 import kubiakdev.com.app.authentication.firebase.util.AuthenticationConst.FIREBASE_JWT_AUTH_KEY
 
 class FirebaseAuthProvider(config: FirebaseConfig) : AuthenticationProvider(config) {
@@ -24,15 +22,13 @@ class FirebaseAuthProvider(config: FirebaseConfig) : AuthenticationProvider(conf
         val token = tokenParser.parseToken(context.call)
 
         if (token == null) {
-            requestForAuthenticateOnNullToken(context)
+            context.call.respond(HttpStatusCode.Unauthorized)
             return
         }
 
         try {
             val principalUser = tokenVerifier.verifyFirebaseIdToken(
-                call = context.call,
-                authHeader = token,
-                principalCreationMethod = principalCreationMethod
+                call = context.call, authHeader = token, principalCreationMethod = principalCreationMethod
             )
 
             if (principalUser != null) {
@@ -43,20 +39,4 @@ class FirebaseAuthProvider(config: FirebaseConfig) : AuthenticationProvider(conf
             context.error(FIREBASE_JWT_AUTH_KEY, AuthenticationFailedCause.Error(message))
         }
     }
-
-    // todo verify that if we can remove it
-    private fun requestForAuthenticateOnNullToken(context: AuthenticationContext) {
-        context.challenge(
-            key = FIREBASE_JWT_AUTH_KEY,
-            cause = AuthenticationFailedCause.InvalidCredentials
-        ) { challengeFunc, call ->
-            challengeFunc.complete()
-            call.respond(
-                UnauthorizedResponse(HttpAuthHeader.bearerAuthChallenge(realm = FIREBASE_AUTH_CONFIGURATION_NAME))
-            )
-        }
-    }
-
-    private fun HttpAuthHeader.Companion.bearerAuthChallenge(realm: String): HttpAuthHeader =
-        HttpAuthHeader.Parameterized(AUTH_SCHEME, mapOf(HttpAuthHeader.Parameters.Realm to realm))
 }
