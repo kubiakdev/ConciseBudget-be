ac016b1687d2c40c83174844d0948d681ad566dd
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2024-03-09 08:45:29 +0100

    Add option for initializing MongoClient.kt by local file

--- src/main/kotlin/kubiakdev/com/data/database/MongoClient.kt
+++ src/main/kotlin/kubiakdev/com/data/database/MongoClient.kt
@@ -2,5 +2,7 @@ package kubiakdev.com.data.database
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
 
-private val client = MongoClient.create(System.getenv("mongo_connection"))
+private val client = MongoClient.create(getMongoConnection())
 val database = client.getDatabase("test")
+
+private fun getMongoConnection(): String = System.getenv("mongo_connection") ?: mongoConnection
