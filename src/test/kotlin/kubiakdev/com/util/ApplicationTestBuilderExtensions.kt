208411ee6dd500107e76832e61225572ce3fbf9d
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2024-03-09 10:19:52 +0100

    Delete test user in case of test failure

--- src/test/kotlin/kubiakdev/com/util/ApplicationTestBuilderExtensions.kt
+++ src/test/kotlin/kubiakdev/com/util/ApplicationTestBuilderExtensions.kt
@@ -15,8 +15,12 @@ import kubiakdev.com.util.provider.json
 fun testWithUserCreation(block: suspend ApplicationTestBuilder.(SignUpResponse) -> Unit) {
     testApplication {
         val signUpResponse = signUpTestUser()
-        block(json.decodeFromString<SignUpResponse>(signUpResponse.bodyAsText()))
-        deleteTestUser(signUpResponse)
+        try {
+            block(json.decodeFromString<SignUpResponse>(signUpResponse.bodyAsText()))
+        } catch (e: Throwable) {
+            deleteTestUser(signUpResponse)
+            throw e
+        }
     }
 }
 
