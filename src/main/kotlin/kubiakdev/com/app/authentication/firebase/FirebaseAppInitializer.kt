5f90f7c70bbb80823e193ab6105b6d8c2cd8a565
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2024-03-16 13:53:32 +0100

    Add firebase adminsdk test and prod variations

--- src/main/kotlin/kubiakdev/com/app/authentication/firebase/FirebaseAppInitializer.kt
+++ src/main/kotlin/kubiakdev/com/app/authentication/firebase/FirebaseAppInitializer.kt
@@ -3,6 +3,8 @@ package kubiakdev.com.app.authentication.firebase
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import kubiakdev.com.domain.util.environment.Environment
+import kubiakdev.com.util.provider.environment.environment
 import java.io.File
 import java.io.InputStream
 
@@ -20,8 +22,14 @@ class FirebaseAppInitializer {
         .build()
 
     private fun getServiceAccount(): InputStream =
-        System.getenv("firebase_adminsdk")?.byteInputStream()
-            ?: File("firebase-adminsdk.json").inputStream()
+        if (environment == Environment.Prod) getProdServiceAccount() else getTestServiceAccount()
+
+    private fun getTestServiceAccount(): InputStream = getServiceAccount(envName = "firebase-adminsdk-test")
+
+    private fun getProdServiceAccount(): InputStream = getServiceAccount(envName = "firebase-adminsdk-prod")
+
+    private fun getServiceAccount(envName: String): InputStream =
+        System.getenv(envName)?.byteInputStream() ?: File("$envName.json").inputStream()
 
     private companion object {
         private const val DATABASE_URL = "https://concisebudget-default-rtdb.europe-west1.firebasedatabase.app"
