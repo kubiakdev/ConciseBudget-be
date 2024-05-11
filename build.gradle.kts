a52f07339f5d20fa60d0c839f1460b2e3f5ccc9c
Patryk  Kubiak <patryk.kubiak@schibsted.com>
2024-05-11 14:46:51 +0200

    Update libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
-
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val koin_ktor: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val koinKtorVersion: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.9.24"
     id("io.ktor.plugin") version "2.3.5"
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.24"
 }
 
 group = "kubiakdev.com"
@@ -22,37 +21,37 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("io.ktor:ktor-server-config-yaml:$ktorVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 
     // Authentication
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
     implementation("com.google.firebase:firebase-admin:9.2.0")
 
     // Koin
-    implementation("io.insert-koin:koin-ktor:$koin_ktor")
-    implementation("io.insert-koin:koin-logger-slf4j:$koin_ktor")
+    implementation("io.insert-koin:koin-ktor:$koinKtorVersion")
+    implementation("io.insert-koin:koin-logger-slf4j:$koinKtorVersion")
 
     // OkHttp
     implementation("io.ktor:ktor-client-okhttp:2.3.9")
 
     // Mongo
-    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.0.0")
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.1.0")
 
     // Rate limiting
-    implementation("io.ktor:ktor-server-rate-limit:$ktor_version")
+    implementation("io.ktor:ktor-server-rate-limit:$ktorVersion")
 
     // Serialization
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
     // Tests
-    testImplementation("io.insert-koin:koin-test:$koin_ktor")
-    testImplementation("io.insert-koin:koin-test-junit5:$koin_ktor")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$koin_ktor")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.insert-koin:koin-test:$koinKtorVersion")
+    testImplementation("io.insert-koin:koin-test-junit5:$koinKtorVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
