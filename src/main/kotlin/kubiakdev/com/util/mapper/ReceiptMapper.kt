da02ae593cbc5e3beafa49b261b6c156ff15e41f
pkubiak <patrk.kubiak@espeo.eu>
2024-06-18 22:01:17 +0200

    Add ReceiptGeminiResponse model

--- src/main/kotlin/kubiakdev/com/util/mapper/ReceiptMapper.kt
+++ src/main/kotlin/kubiakdev/com/util/mapper/ReceiptMapper.kt
@@ -1,5 +1,7 @@
 package kubiakdev.com.util.mapper
 
+import kubiakdev.com.app.document.ReceiptItemGeminiResponse
+import kubiakdev.com.app.document.ReceiptGeminiResponse
 import kubiakdev.com.domain.model.document.Receipt
 import kubiakdev.com.domain.model.document.ReceiptItem
 import kubiakdev.com.route.document.v1.model.ReceiptItemRouteModel
@@ -19,3 +21,20 @@ fun ReceiptItem.toRouteModel() = ReceiptItemRouteModel(
     priceForOne = priceForOne,
     totalDiscount = totalDiscount,
 )
+
+fun ReceiptGeminiResponse.toDomainModel() = Receipt(
+    title = title,
+    date = date,
+    price = parsePrice(price),
+    items = items?.map { it.toDomainModel() }
+)
+
+fun ReceiptItemGeminiResponse.toDomainModel() = ReceiptItem(
+    name = name,
+    category = category,
+    amount = amount,
+    priceForOne = parsePrice(priceForOne),
+    totalDiscount = parsePrice(totalDiscount),
+)
+
+private fun parsePrice(text: String?): Double? = text?.replace(',', '.')?.toDouble()
